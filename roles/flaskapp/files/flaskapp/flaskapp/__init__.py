ea99f12c2be0b475777817d328a9adcc5107384a
Daniel Santos <drsantos20@gmail.com>
2017-08-14 12:17:35 -0300

    minor changes

--- roles/flaskapp/files/flaskapp/flaskapp/__init__.py
+++ roles/flaskapp/files/flaskapp/flaskapp/__init__.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hi, My Name is Daniel Santos'
+    return 'Hi, My Name Flask Application'
 
 if __name__ == '__main__':
     app.run()
